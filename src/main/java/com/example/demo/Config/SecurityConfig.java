825fd5780a79be61389c5aba0fa0955819d3c52f
Hienptt0208 <hienpttse171265@fpt.edu.vn>
2024-07-11 08:08:29 +0700

    Update SecurityConfig

--- src/main/java/com/example/demo/Config/SecurityConfig.java
+++ src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -1,18 +1,6 @@
 package com.example.demo.Config;
 
-import com.example.demo.Security.CustomAuthenticationSuccessHandler;
-import com.example.demo.Security.CustomAuthenticationFailureHandler;
-import com.example.demo.Security.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
+
 
 @Configuration
 @EnableWebSecurity
