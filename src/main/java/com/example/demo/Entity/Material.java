935deddc8ac6850b12c3c84f1ba98ff0ebecac80
Ocean0335 <duongtdse170335@fpt.edu.vn>
2024-09-18 17:57:38 +0700

    Update Material.java

--- src/main/java/com/example/demo/Entity/Material.java
+++ src/main/java/com/example/demo/Entity/Material.java
@@ -5,12 +5,14 @@ import java.util.List;
 import javax.persistence.*;
 import lombok.*;
 import javax.validation.constraints.NotNull;
+import lombok.experimental.FieldDefaults;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Material {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
